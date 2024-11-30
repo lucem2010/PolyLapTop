788facbb7e6eb7f5f171350fb8d89400780cff13
Dinh Tien Luc <lucd7464@gmail.com>
2024-12-01 04:11:05 +0700

    Update code: chỉnh sửa và thêm tệp cấu hình

--- app/src/main/java/model/GHNService.kt
+++ app/src/main/java/model/GHNService.kt
@@ -2,14 +2,14 @@ package model
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object GHNService {
-    private const val BASE_URL = "https://online-gateway.ghn.vn/shiip/public-api/"
-
-    val api: GHNApi by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(GHNApi::class.java)
-    }
-}
+//object GHNService {
+//    private const val BASE_URL = "https://online-gateway.ghn.vn/shiip/public-api/"
+//
+//    val api: GHNApi by lazy {
+//        Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//            .create(GHNApi::class.java)
+//    }
+//}
